531a6fc20a94bf37b56136199eadd404e0bac97b
Owen Jones <theelusivegerbilfish@Owens-MacBook-Pro.local>
2024-11-24 13:11:28 -0800

    boilerplate for http server complete

---
+++ json.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	if code > 499 {
+		log.Println("Responding with 5XX error:", message)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJson(w, code, errorResponse{Error: message})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %v", payload)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
